6ea260d47c019633fcfb77bd0b04d7a66d59871d
Liv928 <livwu928@gmail.com>
2021-04-08 11:03:05 -0400

    currency update

--- angular-9-registration-login-example-master/src/app/home/home.component.ts
+++ angular-9-registration-login-example-master/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component , OnInit } from '@angular/core';
 
+
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 
@@ -9,6 +10,7 @@ import { HighchartsChartModule } from 'highcharts-angular';
 import HighchartsMore from 'highcharts/highcharts-more';
 
 import {MatTabsModule} from '@angular/material/tabs';
+import { StockApiService } from '@app/stock-api.service';
 
 StockModule(Highcharts);
 
@@ -28,44 +30,28 @@ export interface Tile {
     text: string;
   }
 
-  export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-  }
-  const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
     user: User;
     public chartOptions;
-    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-    dataSource = ELEMENT_DATA;
     public Highcharts = Highcharts;
+
     
     tiles: Tile[] = [
         {text: 'One', cols: 3, rows: 5, color: 'lightblue'},
         {text: 'Two', cols: 1, rows: 5, color: 'lightpink'},
         
       ];
-
-    constructor(private accountService: AccountService) {
+    
+    constructor(private accountService: AccountService, private apiService: StockApiService) {
         this.user = this.accountService.userValue;
     }
 
     ngOnInit(){
+        this.apiService.getStocks().subscribe((res)=>{
+          console.log(res);
+        })
         
         this.chartOptions = {
           legend: {
--- angular-9-registration-login-example-master/src/app/stock-api.service.ts
+++ angular-9-registration-login-example-master/src/app/stock-api.service.ts
@@ -6,6 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StockApiService {
   private heroesUrl = 'https://cloud.iexapis.com/stable/stock/googl/quote?displayPercent=true&token=pk_9b3e50aa00274c0a8143cbf1cd6f0048 ';  // URL to web api
+  private curUrl = 'https://cloud.iexapis.com/stable/crypto/btcusdt/quote?token=pk_9b3e50aa00274c0a8143cbf1cd6f0048';
+
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -17,5 +19,9 @@ export class StockApiService {
 
   getStocks(){
       return this.http.get(this.heroesUrl);
+  }
+
+  getCurrency(){
+    return this.http.get(this.curUrl);
   }
 }
