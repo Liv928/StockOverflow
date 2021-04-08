b73600a7ca00e9e8b4ff367ad4ce4bf7d0d96b98
CometWang <cometwang719@gmail.com>
2021-04-09 00:58:12 +0800

    Solve 2d array

--- angular-9-registration-login-example-master/src/app/home/home.component.ts
+++ angular-9-registration-login-example-master/src/app/home/home.component.ts
@@ -10,9 +10,6 @@ import HighchartsMore from 'highcharts/highcharts-more';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import { StockApiService } from '@app/stock-api.service';
-//import { write } from 'fs';
-
-
 
 StockModule(Highcharts);
 
@@ -58,157 +55,78 @@ export class HomeComponent {
     displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
     dataSource = ELEMENT_DATA;
     public Highcharts = Highcharts;
-<<<<<<< HEAD
-    public oneMonthData =[];  //initializ an array for stock data
-    public fetchedData = [];
-    public graphData_x = [];  //oneMonthPrice: number[] = [];
-=======
     //initializ an array for stock data
-    oneMonthDate: string[] =[]; 
-    oneMonthPrice: number[] = [];
-    symbol: string[] = [];
-    tap = false;
-    stockInfo: {symbol: string,name: string,high:string,low:string,volume:string}[] = 
-    [{symbol: 'AAPL',name: 'Apple',high: '117.49',low:'116.22',volume:'46691331'},
-    {symbol: 'A',name: 'Agilent Technologies Inc.',high: '117.49',low:'116.22',volume:'46691331'},
-    {symbol: 'AACG',name: 'ATA Creativity Global - ADR',high: '117.49',low:'116.22',volume:'46691331'},];
->>>>>>> 9aa332a8bd4e0dfee310c5ea611c57d281e8c814
+    public oneMonthData=[]; 
+    //oneMonthPrice: number[] = [];
+    graphData = []; 
 
     tiles: Tile[] = [
         {text: 'One', cols: 3, rows: 5, color: 'lightblue'},
         {text: 'Two', cols: 1, rows: 5, color: 'lightpink'},
+        
       ];
 
     constructor(private accountService: AccountService, private apiService: StockApiService) {
         this.user = this.accountService.userValue;
     }
-<<<<<<< HEAD
-   
     getonemonthData(){
       this.apiService.getonemonthDate().subscribe((res)=>{
-        for (const item in res){
-          let temp = []
-          //var tempDate = new Date(res[item].date);
-          //var tms = Date.UTC(tempDate.getUTCFullYear(),tempDate.getMonth(),tempDate.getDate(),tempDate.getHours(),tempDate.getMinutes());
-          temp.push(res[item].date);
-          temp.push(res[item].close);
-          
-          this.fetchedData.push(temp);
-          console.log("in the loop-----",this.fetchedData);
-        }
-         /*
+        let i = 0;
         for (const item in res){
           // store the pair array [datetime, price]
-          let temp = []
-          //var tempDate = new Date(res[item].date);
-          //var tms = Date.UTC(tempDate.getUTCFullYear(),tempDate.getMonth(),tempDate.getDate(),tempDate.getHours(),tempDate.getMinutes());
-          temp.push(res[item].date)
-          temp.push(res[item].close)
-          
-          this.fetchedData.push(res[item]);
-          console.log(this.fetchedData); 
-          
-          //截止目前正确 oneMonthData 存储的是[date, price]
-          //push the pair into 2-d array
-          //this.graphData.push(this.oneMonthData);
-          */
-=======
-    getonemonthDate(){
-      this.apiService.getonemonthDate().subscribe((res)=>{
-        for (const item in res){
-          this.oneMonthDate.push(res[item].date);
->>>>>>> 9aa332a8bd4e0dfee310c5ea611c57d281e8c814
-          
-        
-      });
-      
-<<<<<<< HEAD
-=======
-    }
-    getoneMonthPrice(){
-      this.apiService.getonemonthDate().subscribe((res)=>{
-        for (const item in res){
-          this.oneMonthPrice.push(res[item].close);
-        }
-      }); 
->>>>>>> 9aa332a8bd4e0dfee310c5ea611c57d281e8c814
-    }
-    
-    getSymbol(){
-      this.apiService.getSymbol().subscribe((res)=>{
-        for (let index = 0; index < 10; index++){
-          this.symbol.push(res[index].symbol);
+          this.graphData[i] = [];
+       
+          this.graphData[i].push(res[item].date);
+          this.graphData[i].push(res[item].close);  
+          //console.log(this.graphData); 
+          i++;
           
         }
-      }); 
-      
+      });   
     }
-    getStock(){
 
-    }
-    clickEvent(){
-      let old = this.tap;
-      this.tap = !old;
-    }
-    
     ngOnInit(){
-<<<<<<< HEAD
-        /*
-        this.apiService.getonemonthDate().subscribe((res)=>{
-          console.log(res)
-          for (const item in res){
-            let temp = []
-            //var tempDate = new Date(res[item].date);
-            //var tms = Date.UTC(tempDate.getUTCFullYear(),tempDate.getMonth(),tempDate.getDate(),tempDate.getHours(),tempDate.getMinutes());
-            temp.push(res[item].date);
-            temp.push(res[item].close);
-            
-            this.fetchedData.push(temp);
-            console.log("in the loop-----"+this.fetchedData);
-          }
-        });*/
-        this.getonemonthData();
-        console.log("out the loop------"+this.fetchedData); 
-        
         //getData -> return value put into html
-        //this.getonemonthData();
+        this.getonemonthData();
         //this.getoneMonthPrice();
         
-        //console.log(this.oneMonthPrice); 
-=======
-        //getData -> return value put into html
-        this.getonemonthDate();
-        this.getoneMonthPrice();
-        this.getSymbol();
-        console.log(this.oneMonthDate); 
-        console.log(this.oneMonthPrice); 
-        console.log(this.symbol); 
->>>>>>> 9aa332a8bd4e0dfee310c5ea611c57d281e8c814
+        console.log(this.graphData); 
         this.chartOptions = {
-          chart:{
+          legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'middle',
+            itemHoverStyle: {
+              color: 'red',
+            }
           },
-          plotOptions: {
+          rangeSelector: {
+            selected: 2
           },
           title: {text: 'Stock Data'},
           series: [{
+            showInLegend: true,
+            type: 'line',
             name: '',
             tooltip: {
-              valueDecimals: 2,
+              valueDecimals: 2
             },
-            data:this.oneMonthData
+            data:[this.graphData]
           }],
           yAxis: {
-            title: {text:'Celsius'}
+            opposite: false,
+            title: {}
           },
           xAxis: {
             type: 'datetime',
             dateTimeLabelFormats: {
-              week: '%Y-%m-%d'
+                hour: '%H:%M'
             },
-           
+            minRange: 1000,
+            minTickInterval: 1000 
           }
         }
+      };
     
-    
-      }
+
 }
