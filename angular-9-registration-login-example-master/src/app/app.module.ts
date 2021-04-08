0dd5ac359948d2eb417a8406734eba2c4f14e2be
Liv928 <livwu928@gmail.com>
2021-04-07 20:32:18 -0400

    home page template

---
+++ angular-9-registration-login-example-master/src/app/app.module.ts
@@ -0,0 +1,65 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+
+
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Exporting from 'highcharts/modules/exporting';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatDialogModule} from '@angular/material/dialog';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        HighchartsChartModule,
+        MatButtonModule ,
+        MatDatepickerModule,
+        MatGridListModule
+
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ angular-9-registration-login-example-master/src/app/home/home.component.ts
@@ -0,0 +1,108 @@
+﻿import { Component , OnInit } from '@angular/core';
+
+import { User } from '@app/_models';
+import { AccountService } from '@app/_services';
+
+import * as Highcharts from 'highcharts';
+import StockModule from 'highcharts/modules/stock';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+import HighchartsMore from 'highcharts/highcharts-more';
+
+StockModule(Highcharts);
+
+Highcharts.setOptions({
+  title: {
+    style: {
+      color: 'black'
+    }
+  }
+});
+
+
+export interface Tile {
+    color: string;
+    cols: number;
+    rows: number;
+    text: string;
+  }
+
+  export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+  }
+  const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    user: User;
+    public chartOptions;
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    dataSource = ELEMENT_DATA;
+    public Highcharts = Highcharts;
+    
+    tiles: Tile[] = [
+        {text: 'One', cols: 3, rows: 3, color: 'lightblue'},
+        {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+        {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+        {text: 'Four', cols: 4, rows: 1, color: '#DDBDF1'},
+      ];
+
+    constructor(private accountService: AccountService) {
+        this.user = this.accountService.userValue;
+    }
+
+    ngOnInit(){
+        
+        this.chartOptions = {
+          legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'middle',
+            itemHoverStyle: {
+              color: 'red',
+            }
+          },
+          rangeSelector: {
+            selected: 2
+          },
+          title: {text: 'Stock Data'},
+          series: [{
+            showInLegend: true,
+            type: 'line',
+            name: '',
+            tooltip: {
+              valueDecimals: 2
+            },
+            data:[]
+          }],
+          yAxis: {
+            opposite: false,
+            title: {}
+          },
+          xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                hour: '%H:%M'
+            },
+            minRange: 1000,
+            minTickInterval: 1000 
+          }
+        }
+      };
+    
+
+}
