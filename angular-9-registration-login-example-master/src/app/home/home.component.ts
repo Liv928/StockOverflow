8e0923dccb970a5c8f354f2f54b528b619b7ea68
Liv928 <livwu928@gmail.com>
2021-04-09 15:33:14 -0400

    home page stock chart

--- angular-9-registration-login-example-master/src/app/home/home.component.ts
+++ angular-9-registration-login-example-master/src/app/home/home.component.ts
@@ -12,6 +12,8 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { StockApiService } from '@app/stock-api.service';
 //import { write } from 'fs';
 
+import data from '../../assets/data.json';
+
 
 
 StockModule(Highcharts);
@@ -24,6 +26,7 @@ Highcharts.setOptions({
   }
 });
 
+//const chartdata = data;
 
 export interface Tile {
     color: string;
@@ -60,6 +63,9 @@ export class HomeComponent{
     public oneMonthData =[];  //initializ an array for stock data
     public fetchedData = [];
     public graphData = [];  //oneMonthPrice: number[] = [];
+
+    public jsonData =data;
+    public chartData = [];
     //initializ an array for stock data
     oneMonthDate: string[] =[]; 
     oneMonthPrice: number[] = [];
@@ -114,31 +120,111 @@ export class HomeComponent{
         console.log(this.oneMonthDate); 
         console.log(this.oneMonthPrice); 
         console.log(this.symbol); 
-        this.chartOptions = {
-          chart:{
-          },
-          plotOptions: {
-          },
-          title: {text: 'Stock Data'},
-          series: [{
-            name: '',
-            tooltip: {
-              valueDecimals: 2,
-            },
-            data:this.oneMonthData
-          }],
-          yAxis: {
-            title: {text:'Celsius'}
-          },
-          xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-              week: '%Y-%m-%d'
-            },
-           
-          }
+
+        this.chartData = this.jsonData.data;
+        console.log(this.chartData);
+        var ohlc = [],
+            volume = [],
+            dataLength = this.chartData.length,
+				// set the allowed units for data grouping
+            groupingUnits = [[
+                'week',                         // unit name
+                [1]                             // allowed multiples
+            ], [
+                'month',
+                [1, 2, 3, 4, 6]
+            ]],
+            i = 0;
+        for (i; i < dataLength; i += 1) {
+            ohlc.push([
+                this.chartData[i][0], // the date
+                this.chartData[i][1], // open
+                this.chartData[i][2], // high
+                this.chartData[i][3], // low
+                this.chartData[i][4] // close
+            ]);
+            volume.push([
+                this.chartData[i][0], // the date
+                this.chartData[i][5] // the volume
+            ]);
         }
-    
-    
+
+        
+        this.chartOptions = {
+          rangeSelector: {
+						selected: 1,
+						inputDateFormat: '%Y-%m-%d'
+				},
+				title: {
+						text: 'Stock Data'
+				},
+				xAxis: {
+						dateTimeLabelFormats: {
+								millisecond: '%H:%M:%S.%L',
+								second: '%H:%M:%S',
+								minute: '%H:%M',
+								hour: '%H:%M',
+								day: '%m-%d',
+								week: '%m-%d',
+								month: '%y-%m',
+								year: '%Y'
+						}
+				},
+				tooltip: {
+						split: false,
+						shared: true,
+				},
+				yAxis: [{
+						labels: {
+								align: 'right',
+								x: -3
+						},
+						title: {
+								text: 'price'
+						},
+						height: '65%',
+						resize: {
+								enabled: true
+						},
+						lineWidth: 2
+				}, {
+						labels: {
+								align: 'right',
+								x: -3
+						},
+						title: {
+								text: 'turnover'
+						},
+						top: '65%',
+						height: '35%',
+						offset: 0,
+						lineWidth: 2
+				}],
+				series: [{
+						type: 'candlestick',
+						name: 'AAPL',
+						color: 'green',
+						lineColor: 'green',
+						upColor: 'red',
+						upLineColor: 'red',
+						tooltip: {
+						},
+						navigatorOptions: {
+								color: Highcharts.getOptions().colors[0]
+						},
+						data: ohlc,
+						dataGrouping: {
+								units: groupingUnits
+						},
+						id: 'sz'
+				},{
+						type: 'column',
+						data: volume,
+						yAxis: 1,
+						dataGrouping: {
+								units: groupingUnits
+						}
+				}]
+        }
       }
 }
